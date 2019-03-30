d6171c262b88bf494402567fd0c74434942ff6f3
BotMaster3000 <jdgebauer@gmx.de>
2019-03-30 23:50:29 +0100

    Initial Commit

---
+++ MasterVillageLibraryTests/Models/VillagerInformationTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MasterVillageLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterVillageLibrary.Models.Tests
+{
+    [TestClass]
+    public class VillagerInformationTests
+    {
+        [TestMethod]
+        public void TestTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
